ec623d57d38de6acbca1fad99c56513cf3f031a2
Niklaus Johner <niklaus.johner@a3.epfl.ch>
2024-10-24 14:01:28 +0200

    Add post-processing script for eggnog annotation tables.
    This will process the output from eggnog and the gene abundances
    and generate tables containing the annotation abundances.

---
+++ post_processing/annotations.py
@@ -0,0 +1,67 @@
+"""
+This script will process the output from eggnog and the gene abundances
+and generate tables containing the annotation abundances.
+"""
+
+import argparse
+import glob
+import os
+
+import numpy as np
+import pandas as pd
+
+
+def annotation_abunances(input_dir, output_dir, old_style):
+    eggnog_file = os.path.join(input_dir, "gene_catalog", "all.emapper.annotations")
+    data = pd.read_csv(eggnog_file, header=4, delimiter="\t", index_col=0)
+    data.where(data != "-", np.nan, inplace=True)
+
+    if old_style:
+        count_dir = "gene_counts"
+    else:
+        count_dir = "gene_counts_gc"
+
+    abundance_files = glob.glob(os.path.join(input_dir, "*", count_dir, "*_genes_per_cell.csv"))
+
+    samples = []
+    for abundance_file in abundance_files:
+        sample = abundance_file.rsplit("/", 1)[-1].rstrip("_genes_per_cell.csv")
+        samples.append(sample)
+        abundance = pd.read_csv(abundance_file, delimiter=",", index_col=0, names=["gene", sample])
+        data = data.join(abundance)
+
+    for colname in ['COG_category', 'GOs', 'EC', 'KEGG_ko', 'KEGG_Pathway',
+                    'KEGG_Module', 'KEGG_Reaction', 'KEGG_rclass', 'BRITE',
+                    'KEGG_TC', 'CAZy', 'BiGG_Reaction', 'PFAMs']:
+        annotation_table = []
+        for gene, row in data[pd.notna(data[colname])].iterrows():
+            annotations = row[colname].split(",")
+            annotation_table.extend([[gene, el] for el in annotations])
+
+        annotation_table = pd.DataFrame(annotation_table, columns=["#query", colname])
+        annotation_table = annotation_table.join(data[samples], on="#query")
+        annotation_table.where(pd.notna(annotation_table), 0, inplace=True)
+        grouped = annotation_table.groupby(colname).sum().drop(columns=["#query"])
+        grouped.to_csv(os.path.join(output_dir, f"{colname}.csv"))
+
+
+if __name__ == '__main__':
+    args = argparse.ArgumentParser("annotations.py")
+    args.add_argument(
+        "input_dir",
+        help="path to the output directory of the pipeline")
+    args.add_argument(
+        "-o", "--output_dir",
+        help="path where the post-processed tables will be written to. "
+             "defaults to post_processed subdirectory in the input_dir")
+    args.add_argument(
+        "--old_style", action="store_true",
+        help="For old versions of the pipeline where abundances for the gene "
+             "catalog are stored in 'gene_catalog' instead of 'gene_catalog_gc'")
+
+    args = args.parse_args()
+    output_dir = args.output_dir or os.path.join(args.input_dir, "post_processed")
+    if not os.path.exists(output_dir):
+        os.mkdir(output_dir)
+
+    annotation_abunances(args.input_dir, output_dir, args.old_style)
