539db22808b6fd68206453ad28a881a091b17587
Niklaus Johner <niklaus.johner@a3.epfl.ch>
2025-02-18 16:10:52 +0100

    Add option to skip the preprocessing folder when filtering out samples.
    This is necessary as we remove the preprocessed_reads folder from
    the output once everything is done for a given sample.

--- pre_processing/filter_samples.py
+++ pre_processing/filter_samples.py
@@ -14,11 +14,20 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger("filter-samples")
 
 
-class SamplesCleaner():
-
-    def __init__(self, samples_file, output_dir):
+class SamplesCleaner:
+    def __init__(self, samples_file, output_dir, ignore_preprocessing):
         self.sample_file = Path(samples_file)
         self.output_dir = Path(output_dir)
+        self.expected_subdirs = [
+            "annotations",
+            "assembly",
+            "gene_counts",
+            "motus",
+            "phanta",
+            "preprocessed_reads",
+        ]
+        if ignore_preprocessing:
+            self.expected_subdirs.remove("preprocessed_reads")
 
     def __call__(self):
         data = pd.read_csv(args.samples_file, header=0)
@@ -26,29 +35,42 @@ class SamplesCleaner():
         samples = data.index
         to_keep = []
         for sample in samples:
-            for subdir in ("annotations", "assembly", "gene_counts", "motus",
-                           "phanta", "preprocessed_reads"):
+            if not Path(self.output_dir, sample).exists():
+                to_keep.append(sample)
+                continue
+            for subdir in self.expected_subdirs:
                 if not Path(self.output_dir, sample, subdir).exists():
                     to_keep.append(sample)
                     logger.info(f"{sample}: missing {subdir}")
                     break
 
         logger.info(f"Keeping {len(to_keep)} samples")
-        outname = Path(self.sample_file.parent, self.sample_file.stem + "_filtered" + self.sample_file.suffix)
+        outname = Path(
+            self.sample_file.parent,
+            self.sample_file.stem + "_filtered" + self.sample_file.suffix,
+        )
         if outname.exists():
-            logger.warning("{} exists already, interrupting")
+            logger.warning(f"{outname} exists already, interrupting")
             sys.exit()
         data[data.index.isin(to_keep)].to_csv(outname, index=True)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     args = argparse.ArgumentParser("clean_up_preprocessing.py")
     args.add_argument(
-        "samples_file",
-        help="path to the input file containing the list of samples")
+        "samples_file", help="path to the input file containing the list of samples"
+    )
     args.add_argument(
-        "-o", "--output_dir", default="output",
-        help="path to the output directory of the pipeline")
+        "-o",
+        "--output_dir",
+        default="output",
+        help="path to the output directory of the pipeline",
+    )
+    args.add_argument(
+        "--ignore_preprocessing",
+        action="store_true",
+        help="remove input even if preprocessed_reads folder is missing",
+    )
 
     args = args.parse_args()
-    SamplesCleaner(args.samples_file, args.output_dir)()
+    SamplesCleaner(args.samples_file, args.output_dir, args.ignore_preprocessing)()
