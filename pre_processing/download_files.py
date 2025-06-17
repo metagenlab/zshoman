ae80c0b417a6b7597cc007e11db29525685d5333
Niklaus Johner <niklaus.johner@a3.epfl.ch>
2025-06-17 09:03:02 +0200

    Auto-format dowload_files.py script.

--- pre_processing/download_files.py
+++ pre_processing/download_files.py
@@ -17,8 +17,7 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger("Post-processing")
 
 
-class FileDownloader():
-
+class FileDownloader:
     def __init__(self, samples_file, input_dir, output_dir):
         self.input_dir = input_dir
         self.output_dir = output_dir
@@ -60,23 +59,33 @@ class FileDownloader():
             p.map(self.download_file, self.files)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     args = argparse.ArgumentParser("download_files.py")
     args.add_argument(
-        "samples_file", type=Path,
-        help="path to the input file containing the list of samples")
+        "samples_file",
+        type=Path,
+        help="path to the input file containing the list of samples",
+    )
     args.add_argument(
-        "input_dir", default="input", type=Path,
-        help="path to the location where the files should get downloaded to")
+        "input_dir",
+        default="input",
+        type=Path,
+        help="path to the location where the files should get downloaded to",
+    )
     args.add_argument(
-        "-o", "--output_dir", default="output", type=Path,
-        help="path to the output directory of the pipeline")
+        "-o",
+        "--output_dir",
+        default="output",
+        type=Path,
+        help="path to the output directory of the pipeline",
+    )
+    args.add_argument("-n", help="number of parallel processes. Default is 10.")
     args.add_argument(
-        "-n",
-        help="number of parallel processes. Default is 10.")
-    args.add_argument(
-        "-f", "--output_samples_file", type=Path,
-        help="ouput sample file name. Defaults to [samples_file]_downloaded.csv")
+        "-f",
+        "--output_samples_file",
+        type=Path,
+        help="ouput sample file name. Defaults to [samples_file]_downloaded.csv",
+    )
 
     args = args.parse_args()
 
@@ -86,15 +95,18 @@ if __name__ == '__main__':
     if not args.output_samples_file:
         args.output_samples_file = Path(
             args.samples_file.parent,
-            args.samples_file.stem + "_downloaded" + args.samples_file.suffix)
+            args.samples_file.stem + "_downloaded" + args.samples_file.suffix,
+        )
 
     with open(args.samples_file) as infile_handle:
         with open(args.output_samples_file, "w") as outfile_handle:
             outfile_handle.write(next(infile_handle))
             for line in infile_handle:
                 res = [el.strip() for el in line.split(",")]
-                res[1:] = [os.path.join(args.input_dir, el.rsplit("/", 1)[-1])
-                           if el else "" for el in res[1:]]
+                res[1:] = [
+                    os.path.join(args.input_dir, el.rsplit("/", 1)[-1]) if el else ""
+                    for el in res[1:]
+                ]
                 outfile_handle.write(", ".join(res) + "\n")
 
     n = int(args.n or 10)
