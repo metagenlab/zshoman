f22148a42b97fa412e888f13d4dc5cc10d8e8f1f
Niklaus Johner <niklaus.johner@a3.epfl.ch>
2025-12-18 16:55:38 +0100

    Improve gene profiling by using coverage instead of number of reads.

--- modules/local/normalize_counts/templates/normalize_counts.py
+++ modules/local/normalize_counts/templates/normalize_counts.py
@@ -3,7 +3,8 @@
 This script counts the number of cells from the motus output and uses that
 as well as the length of the genes to normalize the gene counts.
 """
-from collections import Counter
+
+from statistics import fmean
 
 import pysam
 
@@ -20,19 +21,25 @@ def main():
         for line in handle:
             number_of_cells += int(line.rsplit("\\t", 1)[-1])
 
-    samfile = pysam.AlignmentFile("$aligned_reads", "rb")
-    read_counts = Counter()
-    ref_lengths = {}
-    for read in samfile:
-        name = read.reference_name
-        read_counts[name] += 1
-        if name not in ref_lengths:
-            ref_lengths[name] = read.reference_length
+    pysam.sort("-o", f"{samplename}.bam", "$aligned_reads")
+    pysam.index(f"{samplename}.bam")
+
+    bamfile = pysam.AlignmentFile(f"{samplename}.bam")
+
+    stats = bamfile.get_index_statistics()
+
+    counts = {}
+    for region_stat in stats:
+        if region_stat.total == 0:
+            continue
+        counts[region_stat.contig] = sum(
+            map(fmean, bamfile.count_coverage(region_stat.contig))
+        )
 
     with open(f"{outprefix}_genes_per_cell.csv", "w") as fout:
-        for key, val in read_counts.items():
-            fout.write(f"{key},{val/ref_lengths[key]/number_of_cells}\\n")
+        for key, val in counts.items():
+            fout.write(f"{key},{val / number_of_cells}\\n")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
