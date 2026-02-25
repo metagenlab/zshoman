bd3c07d71920feb09d21e591fcab573a1198b535
Niklaus Johner <niklaus.johner@a3.epfl.ch>
2026-02-25 11:36:29 +0100

    Use argument parser from utils in merge_output.py post-processing script.

--- post_processing/merge_output.py
+++ post_processing/merge_output.py
@@ -3,25 +3,26 @@ This script will gather the outputs (for now mOTUs or phanta) for all samples fr
 nextflow output directory, and merge them into a single table.
 """
 
-import argparse
-import logging
-import os
+import sys
 from functools import reduce
 from pathlib import Path
 
 import pandas as pd
 
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger("Post-processing")
+sys.path.append(str(Path(__file__).parent.parent))
+
+from utils.utils import logger
+from utils.utils import parse_arguments
+
 
 
 class TableMerger:
     to_exclude = ["gene_catalog", "pipeline_info", "logs"]
     out_ext = "csv"
 
-    def __init__(self, samples_file, input_dir, output_dir, prefix):
-        self.input_dir = Path(input_dir)
-        self.output_dir = Path(output_dir)
+    def __init__(self, samples_file, pipeline_outdir, postprocessed_dir, prefix):
+        self.pipeline_outdir = Path(pipeline_outdir)
+        self.postprocessed_dir = Path(postprocessed_dir)
         self.out_prefix = prefix
         self.samples = pd.read_csv(args.samples_file, header=0)["sample"]
         logger.info(f"Found {len(self.samples)} samples.")
@@ -32,7 +33,7 @@ class TableMerger:
     @property
     def outpath(self):
         return Path(
-            self.output_dir, f"{self.out_prefix}_{self.out_name}.{self.out_ext}"
+            self.postprocessed_dir, f"{self.out_prefix}_{self.out_name}.{self.out_ext}"
         )
 
     def filter_samples(self):
@@ -61,7 +62,7 @@ class MotusMerger(TableMerger):
     out_name = "motus"
 
     def get_table_path(self, sample):
-        return Path(self.input_dir, sample, "motus", sample + ".motus")
+        return Path(self.pipeline_outdir, sample, "motus", sample + ".motus")
 
     def load_table(self, sample):
         return pd.read_csv(self.get_table_path(sample), sep="\t", header=2)
@@ -79,7 +80,7 @@ class PhantaMerger(TableMerger):
 
     def get_table_path(self, sample):
         return Path(
-            self.input_dir,
+            self.pipeline_outdir,
             sample,
             "phanta",
             "final_merged_outputs",
@@ -95,36 +96,39 @@ class PhantaMerger(TableMerger):
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser("annotations.py")
-    parser.add_argument("samples_file", help="path to samples csv file.")
-    parser.add_argument(
-        "--input_dir",
-        default="output",
-        help="path to the output directory of the pipeline",
+    others = [
+        {
+            "args": ["--prefix"],
+            "kwargs": {"default": "merged", "help": "prefix for output file names"},
+        },
+        {
+            "args": ["--motus"],
+            "kwargs": {"action": "store_true", "help": "merge motus table"},
+        },
+        {
+            "args": ["--phanta"],
+            "kwargs": {"action": "store_true", "help": "merge phanta table"},
+        }
+        {
+            "args": ["--no_cleanup"],
+            "kwargs": {"action": "store_true", "help": "do not remove empty rows from merged tables"},
+        }
+    ]
+    args = parse_arguments(
+        samples_file="mandatory",
+        pipeline_outdir=True,
+        postprocessed_dir=True,
+        others=others
     )
-    parser.add_argument(
-        "--output_dir", help="path where the merged tables should be written to"
-    )
-    parser.add_argument("--prefix", default="merged", help="prefix for output files")
-    parser.add_argument(
-        "--no_cleanup", action="store_true", help="do not remove empty rows"
-    )
-    parser.add_argument("--motus", action="store_true", help="merge motus table")
-    parser.add_argument("--phanta", action="store_true", help="merge phanta table")
-
-    args = parser.parse_args()
-    output_dir = args.output_dir or os.path.join(args.input_dir, "post_processed")
-    if not os.path.exists(output_dir):
-        os.mkdir(output_dir)
 
     if args.motus:
-        MotusMerger(args.samples_file, args.input_dir, output_dir, args.prefix)(
+        MotusMerger(args.samples_file, args.pipeline_outdir, args.postprocessed_dir, args.prefix)(
             not args.no_cleanup
         )
 
     if args.phanta:
         merger = PhantaMerger(
-            args.samples_file, args.input_dir, output_dir, args.prefix
+            args.samples_file, args.pipeline_outdir, args.postprocessed_dir, args.prefix
         )
         merger("relative_taxonomic_abundance", not args.no_cleanup)
         merger("relative_read_abundance", not args.no_cleanup)
